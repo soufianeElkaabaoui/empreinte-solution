37cc5b9c7cb058b7a330a8a7de15a35a2fa1fac8
Ayoub Khyat <ayoubkhyat@gmail.com>
2022-10-12 23:30:14 +0100

    remove forgetable commentaires and fix href in sidebar for pages

--- resources/views/admin/company.blade.php
+++ resources/views/admin/company.blade.php
@@ -8,7 +8,7 @@
         <div class="row">
             <div class="col d-flex flex-column align-items-center">
                 <div class="card bg-white mr-4 w-100">
-                    {{-- <div class="box-container p-4 d-flex justify-content-around">
+                    <div class="box-container p-4 d-flex justify-content-around">
                     <div class="box">
                         <p> <span class="text-dark text-lg font-weight-bolder">Nom de la société :</span> {{$company->name}}</p>
                         <p> <span class="text-dark text-lg font-weight-bolder">Logo :</span> <img src="{{$company->logo_url}}" alt="Logo de la société"></p>
@@ -20,7 +20,7 @@
                         <p> <span class="text-dark text-lg font-weight-bolder">Heure d'ouverture :</span> {{$company->opening_hour}}</p>
                         <p> <span class="text-dark text-lg font-weight-bolder">Heure de fermeture :</span> {{$company->closing_hour}}</p>
                     </div>
-                  </div> --}}
+                  </div>
                     <!-- END customer-list -->
                 </div> <!-- END card -->
                 <button class="btn bg-gradient-primary shadow-success w-30 mt-4" type="button" data-bs-toggle="modal"
