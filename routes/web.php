d3604db0f849bec8990efe75c9e25408d67766ec
soufiane elkaabaoui <sousou.kaa20@gmail.com>
2022-10-13 15:13:25 +0100

    correct some routes & add Client Home Page route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\admin\CompanyController;
-use App\Http\Controllers\admin\HomeController;
+use App\Http\Controllers\admin\HomeController as AdminHomeController;
 use App\Http\Controllers\admin\MemberController as AdminMemberController;
 use App\Http\Controllers\admin\ProjectController;
 use App\Http\Controllers\admin\ReviewController;
 use App\Http\Controllers\admin\ServiceController as AdminServiceController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\UploadImageController;
@@ -22,27 +23,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function () {
-    return view('/admin/home');
-})->name('Home');
-Route::get('/company', function () {
-    return view('/admin/company');
-})->name('Company');
-Route::get('/members', function () {
-    return view('/admin/members');
-})->name('Members');
-Route::get('/projets', function () {
-    return view('/admin/projects');
-})->name('Projets');
-Route::get('/reviews', function () {
-    return view('/admin/reviews');
-})->name('Reviews');
-Route::get('/service', function () {
-    return view('/admin/service');
-})->name('Services');
-Route::get('/social_links', function () {
-    return view('/admin/social_links');
-})->name('Social Links');
+Route::get('/', [HomeController::class, 'index']);
 Route::get('/log_in', function () {
     return view('/admin/sign_in');
 })->name('log_in');
@@ -63,5 +44,9 @@ Route::resources([
     'companies' => CompanyController::class,
 ]);
 /*--- Admin area's routes ---*/
-Route::get('/master/home', [HomeController::class, 'index'])->name('Dashboard');
+Route::get('/master/home', [AdminHomeController::class, 'index'])->name('Dashboard');
 Route::post('/upload-image', UploadImageController::class);
+Route::get('/contact',function ()
+{
+    return view('contact');
+})->name('contact');
