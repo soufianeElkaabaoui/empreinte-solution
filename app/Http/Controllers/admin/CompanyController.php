a6916dca7541b4e289a54926075b904f0358e689
soufiane elkaabaoui <sousou.kaa20@gmail.com>
2022-10-23 16:33:36 +0100

    add edit company function

--- app/Http/Controllers/admin/CompanyController.php
+++ app/Http/Controllers/admin/CompanyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Models\Company;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class CompanyController extends Controller
 {
@@ -70,7 +71,20 @@ class CompanyController extends Controller
      */
     public function update(Request $request, Company $company)
     {
-        //
+        // check if the file exist && there were no problems uploading the file:
+        if ($request->hasFile('image_url') && $request->file('image_url')->isValid()) {
+            Storage::delete($company->logo_url);
+            $path = $request->file('image_url')->store('images'); // upload the image.
+            $company->logo_url = $path;
+        }
+        $company->name = $request->company_name;
+        $company->phone_number = $request->comapny_phone;
+        $company->email = $request->company_email;
+        $company->adress_location = $request->company_adresse;
+        $company->opening_hour = $request->comapny_open_hour;
+        $company->closing_hour = $request->company_close_hour;
+        $company->save();
+        return back()->with('status', 'Bien modifié.');
     }
 
     /**
