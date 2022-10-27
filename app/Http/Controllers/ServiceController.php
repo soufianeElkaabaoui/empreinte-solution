4acb4f3652168dbe855c702e40cf81161caeaec7
soufiane elkaabaoui <sousou.kaa20@gmail.com>
2022-10-27 19:21:21 +0100

    protect admin routes from foreign users (unauthenticated users)

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
     /**
      * Display a listing of the resource.
      *
