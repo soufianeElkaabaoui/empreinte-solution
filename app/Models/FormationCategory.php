30c602028f2f675b09953e6768f45c254f2c7680
el kaabaoui soufiane <elkaabaoui.soufiane@gmail.com>
2023-07-22 12:26:17 +0100

    setup relationship between formation & category

--- app/Models/FormationCategory.php
+++ app/Models/FormationCategory.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class FormationCategory extends Model
 {
     use HasFactory;
+
+    function formations() : HasMany {
+        return $this->hasMany(Formation::class);
+    }
 }
